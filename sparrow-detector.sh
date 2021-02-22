9a78af3f762e6d6dc9bbf0af0f7f4a926dce6515
Ron Stoner <ron@team.casa>
2021-02-22 10:55:24 -0700

    Remove test file signatures

--- sparrow-detector.sh
+++ sparrow-detector.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 #
 # Sparrow Detector
-# Ron Stoner - @forwardsecrecy
+# Author: Ron Stoner
+# Twitter: @forwardsecrecy
 # Signatures and filenames provided by https://redcanary.com/blog/clipping-silver-sparrows-wings/
 #
 # Version: 1.0
@@ -10,9 +11,9 @@
 # TODO: Add in MD5 hash searches
 
 # File names and MD5 signatures
-FILE_LIST_v1v2=("/tmp/rontest /tmp/rontest2 ~/Library/._insu /tmp/agent.sh /tmp/version.json /tmp/version.plist")
-FILE_LIST_v1=("/tmp/rontest ~/Library/Application Support/agent_updater/agent.sh /tmp/agent ~/Library/Launchagents/agent.plist ~/Library/Launchagents/init_agent.plist")
-FILE_LIST_v2=("/tmp/rontest ~/Library/Application Support/verx_updater/verx.sh /tmp/verx ~/Library/Launchagents/verx.plist ~/Library/Launchagents/init_verx.plist")
+FILE_LIST_v1v2=("~/Library/._insu /tmp/agent.sh /tmp/version.json /tmp/version.plist")
+FILE_LIST_v1=("~/Library/Application Support/agent_updater/agent.sh /tmp/agent ~/Library/Launchagents/agent.plist ~/Library/Launchagents/init_agent.plist")
+FILE_LIST_v2=("~/Library/Application Support/verx_updater/verx.sh /tmp/verx ~/Library/Launchagents/verx.plist ~/Library/Launchagents/init_verx.plist")
 MD5_LIST_v1=("30c9bc7d40454e501c358f77449071aa c668003c9c5b1689ba47a431512b03cc")
 MD5_LIST_v2=("fdd6fb2b1dfe07b0e57d4cbfef9c8149 b370191228fef82635e39a137be470af")
 
